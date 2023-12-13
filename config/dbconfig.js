f820c0b6d807cc1d445fa0bfac6a5b8d1b4b5d2d
Dhruv9544 <dhruvshah905@gmail.com>
2023-12-14 01:44:00 +0530

    config for dbsetup

---
+++ config/dbconfig.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const {MONGO_URI} = process.env
+
+module.exports.getDbconnection = function(req,res){
+
+    mongoose.connect(MONGO_URI).then(()=>console.log("DB connected")).catch((err)=>{
+        console.log(err);
+    })
+}
