9d8e76324d9f62a3fd7de81c203e99f280defa8a
Dhruv9544 <dhruvshah905@gmail.com>
2023-12-14 02:20:11 +0530

    mongo uri added

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const cors = require("cors");
 //dbconnection
 require('./config/dbconfig').getDbconnection()
 
+const app = express()
 
 //middleware
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+
+
+app.listen(9999);
+console.log("server started at 9999");
