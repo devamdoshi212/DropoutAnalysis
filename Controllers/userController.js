dcf2c557569090a08c1aec443cb5bc2e52a234f8
Dhruv9544 <dhruvshah905@gmail.com>
2023-12-14 15:19:57 +0530

    login,signup,token done

---
+++ Controllers/userController.js
@@ -0,0 +1,47 @@
+const UserModel = require("../Models/UserModel");
+const { ACCESS_TOKEN_SECRET } = process.env;
+
+async function signup(req, res) {
+  try {
+    let User = new UserModel(req.body);
+    await User.save();
+    res.json({
+      data: User,
+      rcode: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      data: err,
+      rcode: -9,
+    });
+  }
+}
+
+async function login() {
+  const { Email, Password } = req.body;
+  let User = await UserModel.findOne({ Email: Email });
+
+  if (User && User.Password == Password) {
+    
+    const accesstoken = jwt.sign(
+      { Email: User.Email, Password: User.Password },
+      ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      data: User,
+      msg: "login done",
+      token: accesstoken,
+      rcode: 200,
+    });
+  } else {
+    res.json({ data: req.body, msg: "Invalid credential", rcode: -9 });
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+};
