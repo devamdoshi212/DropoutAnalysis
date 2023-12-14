ec95485e5b8d69065b24e28335d5545cbbd3c346
Devam Doshi <devamdoshi212@gmail.com>
2023-12-15 01:08:21 +0530

    Add School Done

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -5,4 +5,7 @@ const schoolController = require("../controllers/schoolController");
 router.get("/getSchool", schoolController.getSchool);
 router.post("/addSchool", schoolController.addSchool);
 
+router.get("/getSchoolType", schoolController.getSchoolType);
+router.post("/addSchoolType", schoolController.addSchoolType);
+
 module.exports = router;
