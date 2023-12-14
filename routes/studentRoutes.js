dfbab540b97fa357a6e493a264ebb672e38749af
HarshN187 <hnirmal913@gmail.com>
2023-12-14 15:54:33 +0530

    change

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const StudentController = require("../controllers/StudentController");
 
 router.get("/getStudent", StudentController.getStudents);
+router.post("/addStudent", StudentController.addStudents);
 
 module.exports = router;
