6abac9cbb0c369ffd7dde4ab8cfa66ad0e0da077
HarshN187 <hnirmal913@gmail.com>
2023-12-15 23:54:10 +0530

    change in deactive student

--- Controllers/studentController.js
+++ Controllers/studentController.js
@@ -47,6 +47,9 @@ async function deactivateStudent(req, res) {
     const status = req.body.status;
     const id = req.body.students;
     const change = { is_active: status };
+    if (status == 1 || 2) {
+      change.SchoolId = { $push: 0 };
+    }
     if (status == 1) {
       change.Reasons = req.body.reason;
     }
