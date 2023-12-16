d07ba50571ccd297d5fac3f2be5db2b248786e17
Uday Gohel <goheluday6445@gmail.com>
2023-12-16 10:07:58 +0530

    Chart Added in admin

---
+++ client-web/src/Components/Admin/Analysis/IncomewiseDropoutAnalysis.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
+const FamilyIncomewiseDropoutAnalysis = ({
+  selectedCity,
+  selectedTaluka,
+  selectedDistrict,
+  selectedState,
+}) => {
+  const [chartData, setChartData] = useState({
+    series: [
+      {
+        name: "Capacity",
+        data: [30, 40, 35, 50],
+      },
+      {
+        name: "Enroll Athelte",
+        data: [49, 60, 10, 12],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          endingShape: "rounded",
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        offsetY: -10,
+        style: {
+          fontSize: "12px",
+          fontWeight: "bold",
+          colors: ["#000"],
+        },
+      },
+      xaxis: {
+        categories: ["Cricket", "Basket Ball", "Volly Ball", "Tennis"],
+      },
+      yaxis: {
+        title: {
+          text: "Number of Athelte in Sport Complexes",
+          style: {
+            fontSize: "12px",
+            // fontWeight: "bold",
+            fontFamily: undefined,
+            color: "#263238",
+          }, // Your Y-axis title
+        },
+      },
+      colors: ["#66FF33", "#FF3366"],
+      title: {
+        text: "FamilyIncome wise Dropout Analysis",
+        align: "center",
+        margin: 50,
+        offsetX: 0,
+        offsetY: 0,
+        floating: false,
+        style: {
+          fontSize: "26px",
+          fontWeight: "bold",
+          fontFamily: undefined,
+          color: "#263238",
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+    },
+  });
+
+  useEffect(() => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `http://localhost:9999/FilterStudentinGroup/FamilyIncome?state=${selectedState}&district=${selectedDistrict}&city=${selectedCity}&taluka=${selectedTaluka}&school`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log(result);
+        const data = result.data.StudentsData;
+        const categories = data.map((s) => s.FamilyIncome);
+        const student = data.map((s) => s.numOfStudent);
+        setChartData({
+          ...chartData,
+          series: [
+            {
+              data: student,
+            },
+          ],
+          options: {
+            ...chartData.options,
+            xaxis: {
+              ...chartData.options.xaxis,
+              categories: categories,
+            },
+          },
+        });
+      })
+      .catch((error) => console.log("error", error));
+  }, [selectedCity, selectedDistrict, selectedState, selectedTaluka]);
+
+  return (
+    <div className="chart m-8">
+      <ReactApexChart
+        options={chartData.options}
+        series={chartData.series}
+        type="bar"
+        height={chartData.options.chart.height}
+      />
+    </div>
+  );
+};
+
+export default FamilyIncomewiseDropoutAnalysis;
