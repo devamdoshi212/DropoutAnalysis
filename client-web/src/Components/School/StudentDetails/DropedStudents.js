c4ff1130bcdb12e2eac4f4f55087b4050c1ecf6e
Harekrushn Tejani <harekrushntejani64@gmail.com>
2023-12-16 13:32:05 +0530

    Excel download Done in School Side

--- client-web/src/Components/School/StudentDetails/DropedStudents.js
+++ client-web/src/Components/School/StudentDetails/DropedStudents.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Swal from "sweetalert2";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import { FilterMatchMode } from "primereact/api";
@@ -105,26 +105,87 @@ export default function DropedStudents() {
     });
   };
 
+  const dt = useRef(null);
+
+  // console.log(customers);
+  let customerData = [];
+
+  const exportExcel = async () => {
+    await customers.map((customer) => {
+      let newObject = {
+        "AadharNumber": customer.AadharNumber,
+        "Address": customer.Address,
+        "Caste": customer.Caste,
+        "City": customer.City.city,
+        "ContactNumber": customer.ContactNumber,
+        "DOB": customer.DOB,
+        "Disability": customer.Disability ? customer.Disability : 0,
+        "District": customer.District.district,
+        "FamilyIncome": customer.FamilyIncome,
+        "Gender": customer.Gender,
+        "Name": customer.Name,
+        "ParentMaritalStatus": customer.ParentMaritalStatus,
+        "ParentOccupation": customer.ParentOccupation,
+        "SchoolID": customer.SchoolID[0].Name,
+        "Standard": customer.Standard,
+        "State": customer.State.name,
+        "Taluka": customer.Taluka.taluka,
+        "_id": customer._id,
+      }
+      customerData.push(newObject);
+    });
+
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(customerData);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array'
+      });
+
+      saveAsExcelFile(excelBuffer, 'Dropout Student Data');
+    });
+  };
+
+  const saveAsExcelFile = (buffer, fileName) => {
+    import('file-saver').then((module) => {
+      if (module && module.default) {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data = new Blob([buffer], {
+          type: EXCEL_TYPE
+        });
+
+        module.default.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+      }
+    });
+  };
+
   const renderHeader = () => {
     return (
-      <div className="flex justify-between mr-2">
-        <Button
-          type="button"
-          label="Clear"
-          outlined
-          className="px-4 py-2 rounded-lg text-blue-800 ring-0 border-2 border-blue-700 hover:bg-gray-200"
-          onClick={clearFilter}
-        />
-
-        <span className="p-input-icon-left">
-          <InputText
-            value={globalFilterValues.Name}
-            onChange={onGlobalFilterChange}
-            placeholder="Keyword Search"
-            className="p-2 ring-1 ring-opacity-50 ring-black focus:ring-blue-600 focus:ring-2 focus:ring-opacity-70 hover:ring-opacity-100 hover:ring-blue-400"
+      <>
+        <div className="flex align-items-center justify-content-end gap-2">
+          <Button type="button" icon="pi pi-file-excel" severity="success" rounded onClick={exportExcel} data-pr-tooltip="XLS" style={{ backgroundColor: "green" }} />
+        </div>
+        <div className="flex justify-between mr-2">
+          <Button
+            type="button"
+            label="Clear"
+            outlined
+            className="px-4 py-2 rounded-lg text-blue-800 ring-0 border-2 border-blue-700 hover:bg-gray-200"
+            onClick={clearFilter}
           />
-        </span>
-      </div>
+
+          <span className="p-input-icon-left">
+            <InputText
+              value={globalFilterValues.Name}
+              onChange={onGlobalFilterChange}
+              placeholder="Keyword Search"
+              className="p-2 ring-1 ring-opacity-50 ring-black focus:ring-blue-600 focus:ring-2 focus:ring-opacity-70 hover:ring-opacity-100 hover:ring-blue-400"
+            />
+          </span>
+        </div>
+      </>
     );
   };
 
@@ -165,6 +226,7 @@ export default function DropedStudents() {
   return (
     <div className="card p-10">
       <DataTable
+        ref={dt}
         value={customers}
         paginator
         showGridlines
@@ -184,7 +246,7 @@ export default function DropedStudents() {
         <Column
           selectionMode="multiple"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px"}}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
 
         <Column
@@ -195,7 +257,7 @@ export default function DropedStudents() {
             return calculateIndex(Math.floor(first / 10), rowIndex);
           }}
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
 
         <Column
@@ -203,7 +265,7 @@ export default function DropedStudents() {
           field="Name"
           filterField="Name"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           sortable
@@ -211,7 +273,7 @@ export default function DropedStudents() {
           field="UID"
           filterField="UID"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px"}}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           sortable
@@ -219,14 +281,14 @@ export default function DropedStudents() {
           field="Gender"
           filterField="location"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           header="Aadhar Number"
           field="AadharNumber"
           filterField="AadharNumber"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
 
         <Column
@@ -235,7 +297,7 @@ export default function DropedStudents() {
           field="Standard"
           filterField="Standard"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px"}}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           header="DOB"
@@ -243,7 +305,7 @@ export default function DropedStudents() {
           filterField="DOB"
           body={dateBodyTemplate}
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           sortable
@@ -251,14 +313,14 @@ export default function DropedStudents() {
           field="District.district"
           filterField="District"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           header="City"
           field="City.city"
           filterField="City"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           sortable
@@ -266,7 +328,7 @@ export default function DropedStudents() {
           field="Taluka.taluka"
           filterField="Taluka"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           sortable
@@ -274,7 +336,7 @@ export default function DropedStudents() {
           field="Caste"
           filterField="Caste"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
 
         <Column
@@ -282,7 +344,7 @@ export default function DropedStudents() {
           field="City.cityType"
           filterField="City_type"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
 
         <Column
@@ -291,22 +353,22 @@ export default function DropedStudents() {
           field="SchoolID.Medium.name" // Replace 'districtName' with the actual field name
           filterField="School_medium" // Make sure this matches the actual field name
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }} // filterMatchMode={FilterMatchMode.CONTAINS}
-          // filterValue={globalFilterValues.District}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }} // filterMatchMode={FilterMatchMode.CONTAINS}
+        // filterValue={globalFilterValues.District}
         />
         <Column
           header="Address"
           field="Address"
           filterField="Address"
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{ backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
         />
         <Column
           header="Created At"
           field="createdAt" // Replace 'districtName' with the actual field name
           filterField="createdAt" // Make sure this matches the actual field name
           headerStyle={{ color: "#fff", backgroundColor: "#333" }}
-          style={{backgroundColor: "#e9e9e9",border:"solid",borderCollapse:"collapse",borderColor:"#c0c0c0",borderWidth:"1px" }}
+          style={{ backgroundColor: "#e9e9e9", border: "solid", borderCollapse: "collapse", borderColor: "#c0c0c0", borderWidth: "1px" }}
           body={dateBodyTemplate}
         />
       </DataTable>
