19885d12ce608b52af42be09da8c26ed7a188a9c
HarshN187 <hnirmal913@gmail.com>
2023-12-16 22:05:13 +0530

    total counrt in analysis ,top5

--- routes/analysisRoute.js
+++ routes/analysisRoute.js
@@ -15,5 +15,7 @@ router.get("/statewiseDropout", analysisController.statewiseDropout);
 router.get("/mediumWise", analysisController.mediumWise);
 router.get("/areaWise", analysisController.areaWise);
 router.get("/stateWiseCount", analysisController.stateWiseCount);
+router.get("/countryComparative", analysisController.countryComparative);
+router.get("/top5", analysisController.top5);
 
 module.exports = router;
