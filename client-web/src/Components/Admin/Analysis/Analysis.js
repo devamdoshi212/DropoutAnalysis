53e635ba163d44d1c2638d2f61f4c701f477a0f2
Uday Gohel <goheluday6445@gmail.com>
2023-12-16 12:46:09 +0530

    Analysis Done

--- client-web/src/Components/Admin/Analysis/Analysis.js
+++ client-web/src/Components/Admin/Analysis/Analysis.js
@@ -28,135 +28,137 @@ const Analysis = () => {
   }, []);
   return (
     <>
-    <div className="m-5">
-    <div className="flex mb-5">
-      <label className="w-1/4 m-4">
-        <span className="text-gray-500 font-bold w-1/3">Select State</span>
-        <select
-          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
-          value={selectedState}
-          onChange={(e) => {
-            setSelectedState(e.target.value);
-            FetchDistrict(e.target.value).then((res) => {
-              setDistrictName(res);
-            });
-          }}
-          required
-        >
-          <option value="">Select State</option>
-          {stateName.map((item, index) => (
-            <option key={index} value={item._id}>
-              {item.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label className="w-1/4 m-4">
-        <span className="text-gray-500 font-bold w-1/3">Select District</span>
-        <select
-          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
-          value={selectedDistrict}
-          onChange={(e) => {
-            setSelectedDistrict(e.target.value);
-            FetchTaluka(selectedState, e.target.value).then((res) => {
-              setTalukaName(res);
-            });
-          }}
-          required
-        >
-          <option value="">Select District</option>
-          {DistrictName.map((item, index) => (
-            <option key={index} value={item._id}>
-              {item.district}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label className="w-1/4 m-4">
-        <span className="text-gray-500 font-bold w-1/3">Select Taluka</span>
-        <select
-          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
-          value={selectedTaluka}
-          onChange={(e) => {
-            setSelectedTaluka(e.target.value);
-            FetchCity(selectedState, selectedDistrict, e.target.value).then(
-              (res) => {
-                console.log(res);
-                setCityName(res);
-              }
-            );
-          }}
-          required
-        >
-          <option value="">Select Taluka</option>
-          {TalukaName.map((item, index) => (
-            <option key={index} value={item._id}>
-              {item.taluka}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label className="w-1/4 m-4">
-        <span className="text-gray-500 font-bold w-1/3">Select City</span>
-        <select
-          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
-          value={selectedCity}
-          onChange={(e) => {
-            setSelectedCity(e.target.value);
-          }}
-          required
-        >
-          <option value="">Select City/Village</option>
-          {CityName.map((item, index) => (
-            <option key={index} value={item._id}>
-              {item.city}
-            </option>
-          ))}
-        </select>
-      </label>
-      </div>
-      <StandardwiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <GenderwiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <ReasonwiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <CastewiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <DisablitywiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <FamilyIncomewiseDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
-      <ReasonwiseGenderDropoutAnalysis
-        selectedCity={selectedCity}
-        selectedTaluka={selectedTaluka}
-        selectedDistrict={selectedDistrict}
-        selectedState={selectedState}
-      />
+      <div className="m-5">
+        <div className="flex mb-5">
+          <label className="w-1/4 m-4">
+            <span className="text-gray-500 font-bold w-1/3">Select State</span>
+            <select
+              className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+              value={selectedState}
+              onChange={(e) => {
+                setSelectedState(e.target.value);
+                FetchDistrict(e.target.value).then((res) => {
+                  setDistrictName(res);
+                });
+              }}
+              required
+            >
+              <option value="">Select State</option>
+              {stateName.map((item, index) => (
+                <option key={index} value={item._id}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label className="w-1/4 m-4">
+            <span className="text-gray-500 font-bold w-1/3">
+              Select District
+            </span>
+            <select
+              className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+              value={selectedDistrict}
+              onChange={(e) => {
+                setSelectedDistrict(e.target.value);
+                FetchTaluka(selectedState, e.target.value).then((res) => {
+                  setTalukaName(res);
+                });
+              }}
+              required
+            >
+              <option value="">Select District</option>
+              {DistrictName.map((item, index) => (
+                <option key={index} value={item._id}>
+                  {item.district}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label className="w-1/4 m-4">
+            <span className="text-gray-500 font-bold w-1/3">Select Taluka</span>
+            <select
+              className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+              value={selectedTaluka}
+              onChange={(e) => {
+                setSelectedTaluka(e.target.value);
+                FetchCity(selectedState, selectedDistrict, e.target.value).then(
+                  (res) => {
+                    console.log(res);
+                    setCityName(res);
+                  }
+                );
+              }}
+              required
+            >
+              <option value="">Select Taluka</option>
+              {TalukaName.map((item, index) => (
+                <option key={index} value={item._id}>
+                  {item.taluka}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label className="w-1/4 m-4">
+            <span className="text-gray-500 font-bold w-1/3">Select City</span>
+            <select
+              className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+              value={selectedCity}
+              onChange={(e) => {
+                setSelectedCity(e.target.value);
+              }}
+              required
+            >
+              <option value="">Select City/Village</option>
+              {CityName.map((item, index) => (
+                <option key={index} value={item._id}>
+                  {item.city}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+        <StandardwiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <GenderwiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <ReasonwiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <CastewiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <DisablitywiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <FamilyIncomewiseDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <ReasonwiseGenderDropoutAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
       </div>
     </>
   );
