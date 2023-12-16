73b84b62be1c0e49cd07693bf87b14e19c503332
Uday Gohel <goheluday6445@gmail.com>
2023-12-16 10:07:58 +0530

    Chart Added in admin

--- client-web/src/Components/Admin/Analysis/Analysis.js
+++ client-web/src/Components/Admin/Analysis/Analysis.js
@@ -1,139 +1,153 @@
-import React, { useEffect, useState } from "react";
-import ReactApexChart from "react-apexcharts";
+import { useEffect, useState } from "react";
+import FetchState from "../../../API/FetchState";
+import FetchDistrict from "../../../API/FetchDistrict";
+import FetchTaluka from "../../../API/FetchTaluka";
+import FetchCity from "../../../API/FetchCity";
+import StandardwiseDropoutAnalysis from "./StandardwiseDropoutAnalysis";
+import GenderwiseDropoutAnalysis from "./GenderwiseDropoutAnalysis";
+import ReasonwiseDropoutAnalysis from "./ReasonwiseDropoutAnalysis";
+import CastewiseDropoutAnalysis from "./CastewiseDropoutAnalysis";
+import DisablitywiseDropoutAnalysis from "./DisablitywiseDropoutAnalysis";
+import FamilyIncomewiseDropoutAnalysis from "./IncomewiseDropoutAnalysis";
 
-const Analysis = (props) => {
-  const [chartData, setChartData] = useState({
-    series: [
-      {
-        name: "Capacity",
-        data: [30, 40, 35, 50],
-      },
-      {
-        name: "Enroll Athelte",
-        data: [49, 60, 10, 12],
-      },
-    ],
-    options: {
-      chart: {
-        type: "bar",
-        height: 350,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: "55%",
-          endingShape: "rounded",
-        },
-      },
-      dataLabels: {
-        enabled: true,
-        offsetY: -10,
-        style: {
-          fontSize: "12px",
-          fontWeight: "bold",
-          colors: ["#000"],
-        },
-      },
-      xaxis: {
-        categories: ["Cricket", "Basket Ball", "Volly Ball", "Tennis"],
-      },
-      yaxis: {
-        title: {
-          text: "Number of Athelte in Sport Complexes",
-          style: {
-            fontSize: "12px",
-            // fontWeight: "bold",
-            fontFamily: undefined,
-            color: "#263238",
-          }, // Your Y-axis title
-        },
-      },
-      colors: ["#66FF33", "#FF3366"],
-      title: {
-        text: "Capacity wise Enroll Athelte in Sport",
-        align: "center",
-        margin: 50,
-        offsetX: 0,
-        offsetY: 0,
-        floating: false,
-        style: {
-          fontSize: "26px",
-          fontWeight: "bold",
-          fontFamily: undefined,
-          color: "#263238",
-        },
-      },
-      fill: {
-        opacity: 1,
-      },
-    },
-  });
+const Analysis = () => {
+  const [stateName, setStateName] = useState([]);
+  const [TalukaName, setTalukaName] = useState([]);
+  const [DistrictName, setDistrictName] = useState([]);
+  const [CityName, setCityName] = useState([]);
 
-  //   useEffect(() => {
-  //     if (props.selectedOption !== "") {
-  //       var requestOptions = {
-  //         method: "GET",
-  //         redirect: "follow",
-  //       };
-
-  //       fetch(
-  //         `http://localhost:9999/sportCapacityUtilization?sportsComplexId=${props.selectedOption}`,
-  //         requestOptions
-  //       )
-  //         .then((response) => response.json())
-  //         .then((result) => {
-  //           // console.log(result);
-  //           const data = result.data;
-  //           const categories = data.map((s) => parseInt(s.capacity, 10));
-  //           // const seriesData = data.map((s) => s.sport);
-  //           const seriesData = data.map((s) => {
-  //             const utilizationStatus =
-  //               s.capacity < s.totalAthelete ? "Over Utilized" : "Under Utilized";
-  //             const color =
-  //               utilizationStatus === "Under Utilized" ? "#00FF00" : "#FF0000"; // Green for Over Utilized, Red for Under Utilized
-  //             return { label: `${s.sport} - ${utilizationStatus}`, color };
-  //           });
-  //           const totalAthelet = data.map((s) => s.totalAthelete);
-
-  //           setChartData({
-  //             ...chartData,
-  //             series: [
-  //               {
-  //                 data: categories,
-  //               },
-  //               {
-  //                 data: totalAthelet,
-  //               },
-  //             ],
-  //             options: {
-  //               ...chartData.options,
-  //               xaxis: {
-  //                 ...chartData.options.xaxis,
-  //                 categories: seriesData.map((item) => item.label),
-  //                 labels: {
-  //                   style: {
-  //                     fontSize: "16px",
-  //                     fontWeight: "bold",
-  //                     colors: seriesData.map((item) => item.color),
-  //                   },
-  //                 },
-  //               },
-  //             },
-  //           });
-  //         })
-  //         .catch((error) => console.log("error", error));
-  //     }
-  //   }, [props.selectedOption]);
-
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [selectedTaluka, setSelectedTaluka] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  useEffect(() => {
+    FetchState().then((res) => {
+      setStateName(res);
+    });
+  }, []);
   return (
-    <div className="chart m-8">
-      <ReactApexChart
-        options={chartData.options}
-        series={chartData.series}
-        type="bar"
-        height={chartData.options.chart.height}
+    <>
+      <label className="flex mb-4">
+        <span className="text-gray-500 font-bold w-1/3">Select State</span>
+        <select
+          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+          value={selectedState}
+          onChange={(e) => {
+            setSelectedState(e.target.value);
+            FetchDistrict(e.target.value).then((res) => {
+              setDistrictName(res);
+            });
+          }}
+          required
+        >
+          <option value="">Select State</option>
+          {stateName.map((item, index) => (
+            <option key={index} value={item._id}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="flex mb-4">
+        <span className="text-gray-500 font-bold w-1/3">Select District</span>
+        <select
+          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+          value={selectedDistrict}
+          onChange={(e) => {
+            setSelectedDistrict(e.target.value);
+            FetchTaluka(selectedState, e.target.value).then((res) => {
+              setTalukaName(res);
+            });
+          }}
+          required
+        >
+          <option value="">Select District</option>
+          {DistrictName.map((item, index) => (
+            <option key={index} value={item._id}>
+              {item.district}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="flex mb-4">
+        <span className="text-gray-500 font-bold w-1/3">Select Taluka</span>
+        <select
+          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+          value={selectedTaluka}
+          onChange={(e) => {
+            setSelectedTaluka(e.target.value);
+            FetchCity(selectedState, selectedDistrict, e.target.value).then(
+              (res) => {
+                console.log(res);
+                setCityName(res);
+              }
+            );
+          }}
+          required
+        >
+          <option value="">Select Taluka</option>
+          {TalukaName.map((item, index) => (
+            <option key={index} value={item._id}>
+              {item.taluka}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="flex mb-4">
+        <span className="text-gray-500 font-bold w-1/3">Select City</span>
+        <select
+          className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+          value={selectedCity}
+          onChange={(e) => {
+            setSelectedCity(e.target.value);
+          }}
+          required
+        >
+          <option value="">Select City/Village</option>
+          {CityName.map((item, index) => (
+            <option key={index} value={item._id}>
+              {item.city}
+            </option>
+          ))}
+        </select>
+      </label>
+      <StandardwiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
       />
-    </div>
+      <GenderwiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
+      />
+      <ReasonwiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
+      />
+      <CastewiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
+      />
+      <DisablitywiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
+      />
+      <FamilyIncomewiseDropoutAnalysis
+        selectedCity={selectedCity}
+        selectedTaluka={selectedTaluka}
+        selectedDistrict={selectedDistrict}
+        selectedState={selectedState}
+      />
+    </>
   );
 };
 
---
+++ client-web/src/Components/Admin/Analysis/YearwiseGenderAnalysis.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
+const YearwiseGenderAnalysis = (props) => {
+  const [chartData, setChartData] = useState({
+    series: [
+      {
+        name: "Capacity",
+        data: [30, 40, 35, 50],
+      },
+      {
+        name: "Enroll Athelte",
+        data: [49, 60, 10, 12],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          endingShape: "rounded",
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        offsetY: -10,
+        style: {
+          fontSize: "12px",
+          fontWeight: "bold",
+          colors: ["#000"],
+        },
+      },
+      xaxis: {
+        categories: ["Cricket", "Basket Ball", "Volly Ball", "Tennis"],
+      },
+      yaxis: {
+        title: {
+          text: "Number of Athelte in Sport Complexes",
+          style: {
+            fontSize: "12px",
+            // fontWeight: "bold",
+            fontFamily: undefined,
+            color: "#263238",
+          }, // Your Y-axis title
+        },
+      },
+      colors: ["#66FF33", "#FF3366"],
+      title: {
+        text: "Capacity wise Enroll Athelte in Sport",
+        align: "center",
+        margin: 50,
+        offsetX: 0,
+        offsetY: 0,
+        floating: false,
+        style: {
+          fontSize: "26px",
+          fontWeight: "bold",
+          fontFamily: undefined,
+          color: "#263238",
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (props.selectedOption !== "") {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      fetch(
+        `http://localhost:9999/sportCapacityUtilization?sportsComplexId=${props.selectedOption}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          // console.log(result);
+          const data = result.data;
+          const categories = data.map((s) => parseInt(s.capacity, 10));
+          // const seriesData = data.map((s) => s.sport);
+          const seriesData = data.map((s) => {
+            const utilizationStatus =
+              s.capacity < s.totalAthelete ? "Over Utilized" : "Under Utilized";
+            const color =
+              utilizationStatus === "Under Utilized" ? "#00FF00" : "#FF0000"; // Green for Over Utilized, Red for Under Utilized
+            return { label: `${s.sport} - ${utilizationStatus}`, color };
+          });
+          const totalAthelet = data.map((s) => s.totalAthelete);
+
+          setChartData({
+            ...chartData,
+            series: [
+              {
+                data: categories,
+              },
+              {
+                data: totalAthelet,
+              },
+            ],
+            options: {
+              ...chartData.options,
+              xaxis: {
+                ...chartData.options.xaxis,
+                categories: seriesData.map((item) => item.label),
+                labels: {
+                  style: {
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    colors: seriesData.map((item) => item.color),
+                  },
+                },
+              },
+            },
+          });
+        })
+        .catch((error) => console.log("error", error));
+    }
+  }, [props.selectedOption]);
+
+  return (
+    <div className="chart m-8">
+      <ReactApexChart
+        options={chartData.options}
+        series={chartData.series}
+        type="bar"
+        height={chartData.options.chart.height}
+      />
+    </div>
+  );
+};
+
+export default YearwiseGenderAnalysis;
