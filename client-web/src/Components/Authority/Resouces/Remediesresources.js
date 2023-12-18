e42f80891315384eb0f25f73549307c950814593
Harekrushn Tejani <harekrushntejani64@gmail.com>
2023-12-18 22:25:25 +0530

    remedies resources

---
+++ client-web/src/Components/Authority/Resouces/Remediesresources.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+
+const Remediesresources = () => {
+    const [reason, setReason] = useState([]);
+    const [resources, setResources] = useState({});
+
+    const handleReason = (e) => {
+        const id = e.target.value;
+        const reasonData = reason.find(reason => reason._id == id);
+        setResources(reasonData)
+    }
+
+    useEffect(() => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:9999/getReason", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setReason(result.data);
+            })
+            .catch(error => console.log('error', error));
+    }, []);
+    return (
+        <>
+            <div className="relative m-5 w-80">
+                <select
+                    name="dropoutreason"
+                    className="p-2 border border-gray-300 rounded w-full outline-2 focus:outline-gray-500 "
+                    onChange={handleReason}
+                >
+                    <option value="">Choose</option>
+                    {
+                        reason.map((item, index) => (
+                            <option key={index} value={item._id}>{item.reason}</option>
+                        ))
+                    }
+                </select>
+            </div>
+            <div>
+
+
+                {resources && resources.links &&
+                    resources.links.map((resource, index) => (
+                        <a
+                            key={index}
+                            href={resource}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ display: 'block', marginBottom: '5px' }}
+                        >
+                            {resource}
+                        </a>
+                    ))
+                }
+                {resources && resources.resources &&
+                    resources.resources.map((resource, index) => (
+                        <a
+                            key={index}
+                            href={`http://localhost:9999/resources/${resource.file}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ display: 'block', marginBottom: '5px' }}
+                        >
+                            {resource.name}
+                        </a>
+                    ))
+                }
+
+            </div>
+        </>
+    )
+}
+
+export default Remediesresources
