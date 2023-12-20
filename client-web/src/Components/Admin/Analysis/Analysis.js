529939e9ab4d2e68da9eceb38ccb8dea3b338822
Uday Gohel <goheluday6445@gmail.com>
2023-12-20 07:51:16 +0530

    Done

--- client-web/src/Components/Admin/Analysis/Analysis.js
+++ client-web/src/Components/Admin/Analysis/Analysis.js
@@ -14,7 +14,6 @@ import YearwiseGenderAnalysis from "./YearwiseGenderAnalysis";
 import MediumwiseDropoutAnalysis from "./MediumwiseDropoutAnalysis";
 import AreawiseDropoutAnalysis from "./AreawiseDropoutAnalysis";
 import ParentOccupationwiseDropoutAnalysis from "./ParentOccupationwiseDropoutAnalysis";
-import Top5Dropout from "./Top/Top5Dropout";
 import StandardGenderwiseAnalysis from "./StandardGenderwiseAnalysis";
 import ReasonAreawiseAnalysis from "./ReasonAreawiseAnalysis";
 import YearwiseLineChart from "./LineChart/YearwiseChart";
@@ -22,6 +21,11 @@ import ParentEducationwiseAnalysis from "./ParentEducationwiseAnalysis";
 import AcademicsWiseAnalysis from "./AcademicsWiseAnalysis";
 import DropoutReasonwiseTrend from "./LineChart/DropoutReasonwiseTrend";
 import ParentMaritalwiseAnalysis from "./ParentMaritalwiseAnalysis";
+import ReasonwiseCasteAnalysis from "./ReasonwiseCasteAnalysis";
+import AreaReasonwiseAnalysis from "./AreaReasonwiseAnalysis";
+import ReasonwiseFamilyIncomeAnalysis from "./ReasonwiseFamilyIncomeAnalysis";
+
+import FetchReasons from "../../../API/FetchReasons";
 const Analysis = () => {
   const [stateName, setStateName] = useState([]);
   const [TalukaName, setTalukaName] = useState([]);
@@ -34,11 +38,19 @@ const Analysis = () => {
   const [selectedCity, setSelectedCity] = useState("");
   const [standard, setStandard] = useState("");
   const [flag, setflag] = useState("0");
+  const [type, setType] = useState("0");
+
+  const [reason, setReasons] = useState([]);
+  const [rea, setRea] = useState("");
   useEffect(() => {
     FetchState().then((res) => {
       setStateName(res);
     });
+    FetchReasons().then((res) => {
+      setReasons(res);
+    });
   }, []);
+
   return (
     <>
       <div className="m-5">
@@ -164,7 +176,7 @@ const Analysis = () => {
             <span className="text-gray-500 font-bold w-1/3">Select Type</span>
             <select
               className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
-              value={standard}
+              value={flag}
               onChange={(e) => {
                 setflag(e.target.value);
               }}
@@ -172,23 +184,23 @@ const Analysis = () => {
             >
               <option value="">Select Type</option>
               <option value="1">Gender</option>
-              <option value="2">Area</option>
-              <option value="3">Caste</option>
+              <option value="2">Caste</option>
+              <option value="3">Area</option>
               <option value="4">Family Income</option>
             </select>
           </label>
         </div>
 
-        {flag === "0" && <ReasonwiseDropoutAnalysis
-          selectedCity={selectedCity}
-          selectedTaluka={selectedTaluka}
-          selectedDistrict={selectedDistrict}
-          selectedState={selectedState}
-          standard={standard}
-        />
-        }
-        {
-          flag === "1" &&
+        {flag === "0" && (
+          <ReasonwiseDropoutAnalysis
+            selectedCity={selectedCity}
+            selectedTaluka={selectedTaluka}
+            selectedDistrict={selectedDistrict}
+            selectedState={selectedState}
+            standard={standard}
+          />
+        )}
+        {flag === "1" && (
           <ReasonwiseGenderDropoutAnalysis
             selectedCity={selectedCity}
             selectedTaluka={selectedTaluka}
@@ -196,21 +208,101 @@ const Analysis = () => {
             selectedState={selectedState}
             standard={standard}
           />
-        }
+        )}
+        {flag === "2" && (
+          <ReasonwiseCasteAnalysis
+            selectedCity={selectedCity}
+            selectedTaluka={selectedTaluka}
+            selectedDistrict={selectedDistrict}
+            selectedState={selectedState}
+            standard={standard}
+          />
+        )}
+        {flag === "3" && (
+          <AreaReasonwiseAnalysis
+            selectedCity={selectedCity}
+            selectedTaluka={selectedTaluka}
+            selectedDistrict={selectedDistrict}
+            selectedState={selectedState}
+            standard={standard}
+          />
+        )}
+        {flag === "4" && (
+          <ReasonwiseFamilyIncomeAnalysis
+            selectedCity={selectedCity}
+            selectedTaluka={selectedTaluka}
+            selectedDistrict={selectedDistrict}
+            selectedState={selectedState}
+            standard={standard}
+          />
+        )}
 
-
-        <GenderwiseDropoutAnalysis
-          selectedCity={selectedCity}
-          selectedTaluka={selectedTaluka}
-          selectedDistrict={selectedDistrict}
-          selectedState={selectedState}
-        />
+        {/* <label className="w-1/4 m-4">
+          <span className="text-gray-500 font-bold w-1/3">Select State</span>
+          <select
+            className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+            value={rea}
+            onChange={(e) => {
+              setRea(e.target.value);
+            }}
+            required
+          >
+            <option value="">Select Reason</option>
+            {reason.map((item, index) => (
+              <option key={index} value={item._id}>
+                {item.reason}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label className="w-1/4 m-4">
+          <span className="text-gray-500 font-bold w-1/3">Select Type</span>
+          <select
+            className="mt-1 p-2 w-full border rounded-md focus:outline-2 focus:outline-gray-400"
+            value={type}
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+            required
+          >
+            <option value="">Select Type</option>
+            <option value="1">Gender</option>
+            <option value="2">Caste</option>
+            <option value="3">Area</option>
+            <option value="4">Family Income</option>
+          </select>
+        </label> */}
         <ReasonwiseDropoutAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
           selectedDistrict={selectedDistrict}
           selectedState={selectedState}
         />
+        <YearwiseGenderAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <YearwiseLineChart
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <DropoutReasonwiseTrend
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+        <ReasonAreawiseAnalysis
+          selectedCity={selectedCity}
+          selectedTaluka={selectedTaluka}
+          selectedDistrict={selectedDistrict}
+          selectedState={selectedState}
+        />
+
         <CastewiseDropoutAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
@@ -235,12 +327,7 @@ const Analysis = () => {
           selectedDistrict={selectedDistrict}
           selectedState={selectedState}
         />
-        <YearwiseGenderAnalysis
-          selectedCity={selectedCity}
-          selectedTaluka={selectedTaluka}
-          selectedDistrict={selectedDistrict}
-          selectedState={selectedState}
-        />
+
         <MediumwiseDropoutAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
@@ -265,18 +352,7 @@ const Analysis = () => {
           selectedDistrict={selectedDistrict}
           selectedState={selectedState}
         />
-        <ReasonAreawiseAnalysis
-          selectedCity={selectedCity}
-          selectedTaluka={selectedTaluka}
-          selectedDistrict={selectedDistrict}
-          selectedState={selectedState}
-        />
-        <YearwiseLineChart
-          selectedCity={selectedCity}
-          selectedTaluka={selectedTaluka}
-          selectedDistrict={selectedDistrict}
-          selectedState={selectedState}
-        />
+
         <ParentEducationwiseAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
@@ -289,13 +365,14 @@ const Analysis = () => {
           selectedDistrict={selectedDistrict}
           selectedState={selectedState}
         />
-        <DropoutReasonwiseTrend
+
+        <ParentMaritalwiseAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
           selectedDistrict={selectedDistrict}
           selectedState={selectedState}
         />
-        <ParentMaritalwiseAnalysis
+        <GenderwiseDropoutAnalysis
           selectedCity={selectedCity}
           selectedTaluka={selectedTaluka}
           selectedDistrict={selectedDistrict}
