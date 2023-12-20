6515c0c918b646de5de524a9d00ee323322c824b
HarshN187 <hnirmal913@gmail.com>
2023-12-20 12:37:38 +0530

    addresorce

--- Controllers/reasonController.js
+++ Controllers/reasonController.js
@@ -1,11 +1,12 @@
 const ReasonModel = require("./../models/ReasonModel");
+const mongoose = require("mongoose");
 async function addReason(req, res) {
-  let resources = [];
-  req.files.forEach((ele) => {
-    resources.push({ name: ele.originalname, file: ele.filename });
-  });
-  let links = req.body.links;
-  let reason = new ReasonModel({ links, resources, reason: req.body.reason });
+  // let pdf = [];
+  // req.files.forEach((ele) => {
+  //   pdf.push({ name: ele.originalname, file: ele.filename });
+  // });
+  // let links = req.body.links;
+  let reason = new ReasonModel(req.body);
   await reason.save();
   res.json({ rcode: 200 });
 }
@@ -14,7 +15,61 @@ async function getReason(req, res) {
   let data = await ReasonModel.find(req.query);
   res.json({ rcode: 200, data: data });
 }
+
+async function addResource(req, res) {
+  try {
+    // let pdf = [];
+    // req.files["pdf"].map((ele) => {
+    //   pdf.push({ name: ele.originalname, file: ele.filename });
+    // });
+    // let video = [];
+    // req.files["video"].map((ele) => {
+    //   video.push({ name: ele.originalname, file: ele.filename });
+    // });
+    // let pptx = [];
+    // req.files["pptx"].map((ele) => {
+    //   pptx.push({ name: ele.originalname, file: ele.filename });
+    // });
+    let pdf = req.files["pdf"]
+      ? req.files["pdf"].map((ele) => ({
+          name: ele.originalname,
+          file: ele.filename,
+        }))
+      : [];
+    let video = req.files["video"]
+      ? req.files["video"].map((ele) => ({
+          name: ele.originalname,
+          file: ele.filename,
+        }))
+      : [];
+    let pptx = req.files["pptx"]
+      ? req.files["pptx"].map((ele) => ({
+          name: ele.originalname,
+          file: ele.filename,
+        }))
+      : [];
+    // let links = req.body.links;
+    let data = await ReasonModel.findByIdAndUpdate(
+      new mongoose.Types.ObjectId(req.query.id),
+      {
+        pdf: pdf,
+        pptx: pptx,
+        video: video,
+      }
+    );
+    res.json({
+      rcode: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      rcode: -9,
+      err: err.msg,
+    });
+  }
+}
 module.exports = {
   getReason,
   addReason,
+  addResource,
 };
