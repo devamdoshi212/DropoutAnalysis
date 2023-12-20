d9e194f7c0e05fed7df323d1c41bfc1d438109de
HarshN187 <hnirmal913@gmail.com>
2023-12-20 13:19:46 +0530

    channge

--- Models/ReasonModel.js
+++ Models/ReasonModel.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const reasonSchema = new mongoose.Schema({
   reason: String,
   category: [String],
-  resources: [
-    {
-      title: String,
-      keyword: String,
-      standard: Number,
-      pdf: [{ name: String, file: String }],
-      video: [{ name: String, file: String }],
-      pptx: [{ name: String, file: String }],
-      links: [String],
-    },
-  ],
+  resources: {
+    type: [
+      {
+        title: String,
+        keyword: String,
+        standard: Number,
+        pdf: [{ name: String, file: String }],
+        video: [{ name: String, file: String }],
+        pptx: [{ name: String, file: String }],
+        links: [String],
+      },
+    ],
+    default: [],
+  },
 });
 module.exports = mongoose.model("reasons", reasonSchema);
